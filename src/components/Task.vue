c7845d4b1ca6c36ee19cbc9ca333d9a946a347a7
flunkout-dvlpr <info@juliogonzalez.space>
2020-04-03 01:03:19 -0500

    Todo quasar app, first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+    <div>{{ task.name }}</div>
+    <small>{{ task.dueDate }} @ {{ task.dueTime}}</small>
+    <button @click='deleteTask(index)'>x</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['task', 'index']
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-page padding>
+  <ul>
+    <task
+    v-for='(task, index) in tasks'
+    :key="task.id"
+    :task='task'
+    :index='index'
+    :deleteTaks= 'deleteTask'
+    >
+    </task>
+
+  </ul>
+  </q-page>
+</template>
+
+<script>
+import Task from 'components/Task.vue'
+export default {
+  data () {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Learn Vue.js',
+          dueDate: '04-10-2020',
+          dueTime: '12:00'
+        },
+        {
+          id: 2,
+          name: 'Learn Quasar Framework',
+          dueDate: '04-12-2020',
+          dueTime: '12:00'
+        },
+        {
+          id: 3,
+          name: 'Learn AWS Cognito',
+          dueDate: '04-14-2020',
+          dueTime: '12:00'
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTask (index) {
+      this.tasks.splice(index, 1)
+    }
+  },
+  components: {
+    task: Task
+  }
+}
+</script>
+
+<style>
+</style>
